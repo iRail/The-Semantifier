a53c819209dc03d3f6f525d27bd7c31d1189f3b6
Miel Vander Sande <miel.vandersande@ugent.be>
2011-12-12 15:53:07 +0100

    Config update

---
+++ Config.custom.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file contains username and password of your TDT
+ *
+ * @author Pieter Colpaert
+ * @copyright 2011 iRail vzw/asbl
+ */
+
+class Config{
+    
+    public static $HOSTNAME = "";
+    //add trailing slash
+    public static $SUBDIR = "";
+    
+    public static $API_USER = "";
+    public static $API_PASSWD = "";
+        
+}
+
+
+
+?>
